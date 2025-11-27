544e81fe5de371af217db7d711d1dd2ef174f472
Ahmad Hassan <ahmadabuulhassan@gmail.com>
2025-11-28 02:53:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,10 +40,6 @@ gh = GitHubService(
     repo_owner=os.getenv("repo_owner"),
     repo_name=os.getenv("repo_name")
 )
-print("Token:", os.getenv("github_token"))
-print("Repo owner:", os.getenv("repo_owner"))
-print("Repo name:", os.getenv("repo_name"))
-
 # ---------------------------
 # Signup
 # ---------------------------
